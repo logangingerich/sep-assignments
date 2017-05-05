90c6ac43a7235bcb55490e13513ae67ad01418e9
Logan Gingerich <logangingerich@yahoo.com>
2017-05-05 09:55:18 -0600

    line assignment complete

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,45 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+    members.delete(person)
   end
 
   def front
+    members[0]
   end
 
   def middle
+    members[members.length / 2]
   end
 
   def back
+    members[members.length - 1]
   end
 
   def search(person)
+    members.each do |member|
+      if member == person
+        return member
+      end
+    end
+    return nil
   end
 
   private
 
   def index(person)
+    count = 0
+    members.each do |member|
+      if person == member
+        count
+      else
+        count += 1
+      end
+    end
   end
 
 end
