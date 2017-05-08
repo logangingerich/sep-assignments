d3eeef1fe6ee7051feadb14240c1ff3ba35092a4
Logan Gingerich <logangingerich@yahoo.com>
2017-05-08 11:25:55 -0600

    Checkpoint 2 assignments complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,26 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    @queue.unshift(element)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    temp = @queue[0]
+    @queue.each_with_index do |item, index|
+      index -= 1
+    end
+    @queue.delete(temp)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def empty?
+    @queue.length == 0
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = @stack[@stack.length - 1]
   end
 
   def pop
+    temp = self.top
+    @stack.delete(temp)
+    self.top = @stack.last
+    return temp
   end
 
   def empty?
+    @stack.length == 0
   end
 end
